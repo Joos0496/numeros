97a2a990915d78979e9b36985287a42207b844cb
unknown <osmanjarquin20@gmail.com>
2023-09-22 13:26:53 -0600

    tarea 1 de programacion 2

---
+++ src/numerossinre/NumerosSinRe.java
@@ -0,0 +1,34 @@
+
+package numerossinre;
+import javax.swing.*;
+
+public class NumerosSinRe {
+
+    public static void main(String[] args) {
+                  
+        int i, j;
+        
+        int nume[] = new int[6];
+        
+        for (i = 0; i < nume.length; i++){
+            nume[i] = Integer.parseInt(JOptionPane.showInputDialog(null,
+          "Digite el valor para el vector " + "[" + i + "]"));
+        }
+            String e ="";
+            for (j = 0; j < i; j++){
+            String name = e += nume[j];
+{
+                    i = j;
+                    break;
+                }
+        }
+            if (i = 0){
+                nume[i] = e;      
+            } else {
+                
+            }
+        }
+    }
+
+
+    
